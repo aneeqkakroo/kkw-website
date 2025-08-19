d5bd90e786eee6d17d2f651775a0cc5bcfb47c04
Khawaja Aneeq <aneeq.kakroo@gmail.com>
2025-08-19 20:56:02 +0100

    Add Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { Analytics } from "@vercel/analytics/react";
 import { AppRoutes } from "./router/routes";
 
 export default function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <AppRoutes />
+      <Analytics />   {/* 👈 Tracks all page views */}
+    </>
+  );
 }
